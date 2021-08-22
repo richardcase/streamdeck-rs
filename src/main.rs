ca8d8909129d5086363e5098d491579b5f0e8849
Richard Case <richard@weave.works>
2021-08-22 18:19:12 +0100

    work in progress
    Signed-off-by: Richard Case <richard@weave.works>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate simplelog;
-use simplelog::{TermLogger, LevelFilter, TerminalMode, ColorChoice};
+use simplelog::{ColorChoice, LevelFilter, TermLogger, TerminalMode};
 
-use streamdeck::{StreamDeck, Filter, pids};
+use streamdeck::{Filter, StreamDeck};
 
 extern crate structopt;
 use structopt::StructOpt;
+
+extern crate hidapi;
 
 #[derive(StructOpt)]
 #[structopt(name = "streamdeck-rs", about = "a streamdeck cli")]
@@ -19,25 +22,45 @@ struct Options {
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let opts = Options::from_args();
 
     let mut config = simplelog::ConfigBuilder::new();
     config.set_time_level(LevelFilter::Off);
 
-    TermLogger::init(opts.level, config.build(), TerminalMode::Mixed, ColorChoice::Auto).unwrap();
+    TermLogger::init(
+        opts.level,
+        config.build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )
+    .unwrap();
+
+    let hid_api = hidapi::HidApi::new().unwrap();
+    for device in hid_api.device_list() {
+        println!("{:#?}", device)
+    }
+
+    let dev = hid_api.device_list().next().unwrap();
 
     // let mut deck = match StreamDeck::connect(opts.filter.vid, opts.filter.pid, opts.filter.serial) {
-    let mut deck = match StreamDeck::connect(opts.filter.vid, pids::ORIGINAL_V2, opts.filter.serial) {
+    let mut deck = match StreamDeck::connect_with_hid(
+        &hid_api,
+        dev.vendor_id(),
+        dev.product_id(),
+        opts.filter.serial,
+    ) {
         Ok(d) => d,
         Err(e) => {
             error!("error connecting to streamdeck: {:?}", e);
-            return
+            return;
         }
     };
 
     let serial = deck.serial().unwrap();
-    info!("Connected to device (vid: {:04x} pid: {:04x} serial: {})",
-            opts.filter.vid, opts.filter.pid, serial);
+    info!(
+        "Connected to device (vid: {:04x} pid: {:04x} serial: {})",
+        opts.filter.vid, opts.filter.pid, serial
+    );
+
+    deck.set_button_text()
 }
